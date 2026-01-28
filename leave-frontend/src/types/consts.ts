bf6966b36e933f5d1fa4889fc135dd5e7c1a585a
yasmine mizouri <yasminemizouri7@gmail.com>
2026-01-28 16:22:41 +0100

    Frontend build stage

--- leave-frontend/src/types/consts.ts
+++ leave-frontend/src/types/consts.ts
@@ -1,1 +1,3 @@
-export const API_BASE = "http://localhost:8080/leave-management-backend/api";
+export const API_BASE =
+    import.meta.env.VITE_API_BASE_URL ??
+    "http://localhost:8080/leave-management-backend/api";
